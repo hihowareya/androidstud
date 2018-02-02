e39b75d30bca54eefb700903691fa4ed1d2f0313
hihowareya <31700238+hihowareya@users.noreply.github.com>
2018-02-02 11:01:30 -0600

    Create GameObject.java

---
+++ GameObject.java
@@ -0,0 +1,13 @@
+package com.example.woo1765.game;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by woo1765 on 2/2/2018.
+ */
+
+public interface GameObject
+{
+    public void draw(Canvas canvas);
+    public void update();
+}
