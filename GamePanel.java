7fa40dfa2931108cf0fc600603e16a86702254ab
hihowareya <31700238+hihowareya@users.noreply.github.com>
2018-02-08 10:53:33 -0600

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -19,6 +19,8 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
 
     private RectPlayer player;
     private Point playerPoint;
+    private ObstacleManager obstacleManager;
+
     public GamePanel(Context context)
     {
         super(context);
@@ -28,6 +30,9 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
 
         player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(0,0,255));
         playerPoint = new Point(150, 150);
+
+        obstacleManager = new ObstacleManager(200, 350, 75, Color.BLUE);
+
         setFocusable(true);
     }
 
@@ -80,6 +85,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
     public void update()
     {
         player.update(playerPoint);
+        obstacleManager.update();
     }
 
     @Override
@@ -89,5 +95,6 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
 
         canvas.drawColor(Color.WHITE);
         player.draw(canvas);
+        obstacleManager.draw(canvas);
     }
 }
